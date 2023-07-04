1f06f03fbc9069104b59b4bf69947b25992fb230
Abdul Jaweed <jdgaming7320@gmail.com>
2023-07-04 22:21:28 +0530

    updated

---
+++ pages/3_SHALEEN_MISHRA.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+st.header(":blue[Project 1]")
+
+st.title(":red[Market Segmentation]")
+
+st.markdown("""
+            One or more electric motors are used in an electric vehicle (EV) to provide propulsion.
+It can be run by power from extravehicular means through a collector system, or it can run on a battery that can run on its own sometimes after being charged by solar panels or other sources. fuel cells or a generator are used to convert fuel to electricity. EVs can be electric cars, electric trains, electric boats, electric planes, and electric spacecraft, among others.
+In India, the electric car sector is in its infancy. It accounts for less than 1% of total car sales but has the potential to rise to more than 5% in a few years. There are currently about 5 lac electric two-wheelers and a few thousand electric cars on Indian roads. Volumes in the industry have been variable, owing primarily to government incentives. Many significant players (Hero Eco, Ather, Electrotherm, Avon, Lohia, Ampere, and others) are continuing the effort and attempting to impose constructive change.
+The combustion of fossil fuels in traditional automobiles emits toxic pollutants such nitrogen oxides, particulate matter, and volatile organic compounds, which contribute to air pollution and adverse health effects. EVs assist enhance air quality by removing tailpipe emissions, resulting in greater respiratory health and a lower risk of respiratory infections.
+Electric vehicles are needed because they have the ability to drastically reduce emissions, improve air quality, provide energy security, and advance sustainable mobility. By adopting EVs, societies can benefit from environmental and health benefits while also fostering technological innovation and building a cleaner, more sustainable future.
+
+### Dataset [link](https://drive.google.com/file/d/1e1TJacooByJELBd7nmZbtVk_au4KotZ2/view?usp=drive_link)
+            
+            """)
+
+
+st.image("images/shaleen3.png")
+    
+
+st.image("images/shaleen4.png")
+
+
+
+
+st.markdown("""
+            #### Code [link](https://github.com/srvapm/marketsegmentation)
+            """)
+
+
+
+
+st.header(":blue[Project 2]")
+
+st.title(":red[Electr Evehda]")
+
+st.markdown("""
+            #### Dataset [link](https://drive.google.com/file/d/1veBO46wSBwtmED9V8DhEMhabK8TwFzy3/view)
+            """)
+
+
+
+st.image("images/shaleen8.png")
+    
+
+st.image("images/shaleen7.png")
+
+
+
+
+
+
+st.markdown("""
+            #### Code [link](https://github.com/srvapm/ElectrEvehda/blob/b3e7215070ebf22c5608c8fac3ecd4a914769eb3/eda_on_electric_vehicle_datasets(fynn_lab_2_2).py)
+            """)
