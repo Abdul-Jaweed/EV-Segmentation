ac04166b4f26abf419a1db97224c66d2e72c1389
Abdul Jaweed <jdgaming7320@gmail.com>
2023-07-05 02:00:37 +0530

    updated

---
+++ pages/2_ASHVATH_SURESH.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+st.header(":blue[Project 1]")
+
+st.title(":red[Public Charging Stations]")
+
+
+st.markdown("""
+
+Public charging stations are specially designed locations where electric vehicles (EVs) can be recharged. These stations provide a convenient and accessible way for EV owners to replenish their vehicle's battery while away from home. Public charging stations are equipped with charging equipment and connectors that are compatible with various types of EVs. They are often located in public parking lots, shopping centers, and other frequented areas. Public charging stations play a crucial role in supporting the adoption and use of EVs by providing a reliable infrastructure that extends the driving range and enhances the convenience of EV ownership for both kids and adults.
+
+### Dataset [link](https://github.com/A2162014/Feynn-Lab-Internship/blob/main/Project%202/Codes/Code-2/EV-datasets/Details_of_Public_Charging_Stations_Installed.xlsx)
+
+            """)
+
+
+
+
+
+st.image("images/asv1.PNG")
+    
+
+st.image("images/asv2.PNG")
+
+
+
+st.markdown("""
+            #### Code [link](https://github.com/Abdul-Jaweed/Feynn-Lab-Internship/blob/main/Project%202.1/Indian%20EV%20Segmentation.ipynb)
+            """)
+
+
+
+
+
+
+
+
+
+
+st.header(":blue[Project 2]")
+
+st.title(":red[Behavioural Segment Data]")
+
+
+st.markdown("""
+
+Behavioral segment data refers to information that identifies and categorizes individuals based on their behaviors, actions, and interactions with products, services, or brands. This data helps businesses gain insights into consumer preferences, purchase patterns, decision-making processes, brand loyalty, and engagement. By analyzing behavioral segment data, businesses can tailor their marketing strategies, personalize customer experiences, and create targeted campaigns that resonate with specific behavioral segments. This enables businesses to optimize their offerings, improve customer satisfaction, and drive desired behaviors and outcomes.
+
+
+### Dataset [link](https://github.com/A2162014/Feynn-Lab-Internship/blob/main/Project%202/Codes/Code-2/EV-datasets/behavioural_segment_data.csv)
+
+            """)
+
+
+
+
+st.image("images/asv3.png")
+    
+
+st.image("images/asv4.png")
+
+
+st.markdown("""
+            #### Code [link](https://github.com/Abdul-Jaweed/Feynn-Lab-Internship/blob/main/Project%202.1/Indian%20EV%20Segmentation.ipynb)
+            """)
---
+++ pages/4_YASHASWINI.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.title(":red[EV Two Wheeler]")
+
+st.markdown("""
+
+An Electric Vehicle (EV) two-wheeler is a sustainable and efficient mode of transportation that offers a greener alternative to traditional gasoline-powered two-wheelers. With an electric powertrain comprising an electric motor and battery pack, EV two-wheelers produce zero emissions, reduce air pollution, and operate silently. They provide a smooth and instant acceleration experience while offering cost savings through lower operational and maintenance costs. The growing market for EV two-wheelers is driven by increasing environmental concerns and government incentives, making them an appealing choice for eco-conscious individuals looking for a sustainable and economical mode of urban commuting and short-distance travel.
+
+
+### Dataset [link](https://www.kaggle.com/datasets/krishnendubarman/indian-ev-segmentation)
+
+            """)
+
+
+
+
+
+st.image("images/yas1.PNG")
+    
+
+st.image("images/yas2.PNG")
+
+
+
+st.markdown("""
+            #### Code [link](https://github.com/Yashaswini29026/EV__market_segmentation)
+            """)
