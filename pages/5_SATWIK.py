b5c9152508f81ae9d475ea80a98af54713738490
Abdul Jaweed <jdgaming7320@gmail.com>
2023-07-04 20:16:53 +0530

    updated

---
+++ pages/5_SATWIK.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+st.title(":red[Geographic Segmentation on Electrical Vehicles in India]")
+
+st.markdown("""
+            This market segmentation task defines the geographic element of Electrical Vehicles in India where we can choose suitable place in India to set up a stratup according to recent statewise sales of Electrical Vehicles over last 5 years 
+
+### Dataset [link](https://www.kaggle.com/datasets/fathimaibrahimkunju/electric-vehicle-in-india-2022)
+            """)
+
+
+
+st.image("images/satwik1.jpg")
+
+st.image("images/satwik2.jpg")
+
+st.image("images/satwik3.jpg")
+
+st.image("images/satwik4.jpg")
+
+
+
+st.markdown("""
+            #### Code [link](https://github.com/satwikwrites7/My-Machine-Learning-Projects-/blob/b6b4232292a003a3f71ebd44708ee54a538eaccc/EDA%20on%20Electric%20Vehicle%20in%20India%202022.ipynb)
+            """)
